63a26820f974c8405394e67ecafa722c06fa0e8a
Tim Richardson <software@potton.me.uk>
2017-08-06 12:35:16 +0100

    Completed LEDStrip class
    Added in functions for red, gree, blue, white, off and set colour by hex

--- ledstrip.py
+++ ledstrip.py
@@ -1,23 +1,56 @@
 #!/usr/bin/python
+"""
+LEDStrip Class
+--------------
+A Python class that drives the Open-Smart RGB LED Strip from a Raspberry Pi.
+Hardware Obtained from http://www.dx.com/p/full-color-rgb-led-strip-driver-module-for-arduino-blue-black-314667
+
+Code originally developed by Philip Leder (https://github.com/schlank/Catalex-Led-Strip-Driver-Raspberry-Pi)
+
+Pin Connections
+Choose any two GPIO Pins; one to provide the Clock signal (CLK), the other the Data (DAT)
+
+Pi    Open-Smart Controller
+Gnd   Gnd
++5v   Vcc
+DAT   Din
+CLK   Cin
+
+Place this file in the same directory as your code.
+In your code, import the file:
+    from ledstrip import LEDStrip
+
+Create a new LED Strip which uses your chosen pins (CLK and DAT) with, e.g.:
+    CLK = 17
+    DAT = 18
+    strip = LEDStrip(CLK, DAT)
+
+Set the colour of the LED strip with
+    strip.setcolor(red, green, blue):
+
+The following methods are public:
+    setcolourrgb(r, g, b) - Sets the LED strip to colour rgb where r, g, b are in the range 0 to 255
+    setcolourwhite() - Sets the strip to white
+    setcolourred() - Sets the strip to Red
+    setcolourgreen() - Sets the strip to Green
+    setcolourblue() - Sets the strip to Blue
+    setcolouroff() - Turns the strip off
+    setcolourhex('hex') - Sets the LED strip to the hex colour 'hex' in range '000000' to 'FFFFFF'
+"""
+
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
 
 class LEDStrip:
-
     def __init__(self, clock, data):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
         self.__clock = clock
         self.__data = data
         self.__delay = 0
         GPIO.setup(self.__clock, GPIO.OUT)
         GPIO.setup(self.__data, GPIO.OUT)
-
-    @staticmethod
-    def cleanup():
-        GPIO.cleanup()
-
 
     def __sendclock(self):
         GPIO.output(self.__clock, False)
@@ -25,13 +58,22 @@ class LEDStrip:
         GPIO.output(self.__clock, True)
         time.sleep(self.__delay)
 
-
     def __send32zero(self):
         for x in range(32):
             GPIO.output(self.__data, False)
-            Clock()
+            self.__sendclock()
 
-    @staticmethod
+    def __senddata(self, dx):
+        self.__send32zero()
+        for x in range(32):
+            if ((dx & 0x80000000) != 0):
+                GPIO.output(self.__data, True)
+            else:
+                GPIO.output(self.__data, False)
+            dx <<= 1
+            self.__sendclock()
+        self.__send32zero()
+
     def __getcode(self, dat):
         tmp = 0
         if ((dat & 0x80) == 0):
@@ -40,8 +82,7 @@ class LEDStrip:
             tmp |= 0x01
         return tmp
 
-
-    def setcolor(self, red, green, blue):
+    def setcolourrgb(self, red, green, blue):
         dx = 0
         dx |= 0x03 << 30
         dx |= self.__getcode(blue)
@@ -54,14 +95,33 @@ class LEDStrip:
 
         self.__senddata(dx)
 
+    def setcolourwhite(self):
+        self.setcolourrgb(255, 255, 255)
 
-    def __senddata(self, dx):
-        self.__send32zero()
-        for x in range(32):
-            if ((dx & 0x80000000) != 0):
-                GPIO.output(self.__data, True)
-            else:
-                GPIO.output(self.__data, False)
-            dx <<= 1
-            self.__sendclock()
-        self.__send32zero()
+    def setcolouroff(self):
+        self.setcolourrgb(0, 0, 0)
+
+    def setcolourred(self):
+        self.setcolourrgb(255, 0, 0)
+
+    def setcolourgreen(self):
+        self.setcolourrgb(0, 255, 0)
+
+    def setcolourblue(self):
+        self.setcolourrgb(0, 0, 255)
+
+    def setcolourhex(self, hex):
+        print('Hex')
+        try:
+            hexcolour = int(hex, 16)
+            red = int((hexcolour & 255 * 255 * 255) / (255 * 255))
+            green = int((hexcolour & 255 * 255) / 255)
+            blue = hexcolour & 255
+            self.setcolourrgb(red, green, blue)
+        except:
+            hexcolour = 0
+            print("Error converting Hex input (%s) a colour." % hex)
+
+    def cleanup(self):
+        self.setcolouroff()
+        GPIO.cleanup()
--- runledstrip.py
+++ runledstrip.py
@@ -1,47 +1,46 @@
 #!/usr/bin/python
-import ledstrip
+from ledstrip import LEDStrip
 import time
 
-CLK = 11
-DAT = 12
+CLK = 17
+DAT = 18
 
 strip = LEDStrip(CLK, DAT)
 
 print("All 0")
-strip.setcolor(0, 0, 0)
+strip.setcolouroff()
 # time.sleep(1)
-print("All 255")
-strip.setcolor(255, 255, 255)
-time.sleep(.2)
-print("Red 255")
-strip.setcolor(255, 0, 0)
-time.sleep(.2)
-print("Green 255")
-strip.setcolor(0, 255, 0)
-time.sleep(.2)
-print("Blue 255")
-strip.setcolor(0, 0, 255)
-time.sleep(.2)
-print("All 0")
-strip.setcolor(0, 0, 0)
-time.sleep(.2)
+# print("All 255")
+# strip.setcolourwhite()
+# time.sleep(1)
+# print("Red 255")
+# strip.setcolourred()
+# time.sleep(1)
+# print("Green 255")
+# strip.setcolourgreen()
+# time.sleep(1)
+# print("Blue 255")
+# strip.setcolourblue()
+# time.sleep(1)
+print("FFFFFF")
+strip.setcolourhex("FFFFFF")
+time.sleep(1)
+print("XADA55")
+strip.setcolourhex("XADA55")
+time.sleep(1)
+#
+# for x in range(255):
+#     strip.setcolourRGB(x, x, x)
+# for x in range(255):
+#     strip.setcolourRGB(255 - x, 255 - x, 255 - x)
+#
+# for x in range(20):
+#     strip.setcolourRGB(255, 0, 0)
+#     time.sleep(0.1)
+#     strip.setcolourRGB(0, 0, 255)
+#     time.sleep(0.1)
 
-for z in range(10):
-    for x in range(255):
-        # print("X:%d" % (x)
-        # y=x*10
-        strip.setcolor(x, x, x)
-    for x in range(255):
-        strip.setcolor(255 - x, 255 - x, 255 - x)
 
-for x in range(20):
-    strip.setcolor(255, 0, 0)
-    time.sleep(0.1)
-    strip.setcolor(0, 0, 255)
-    time.sleep(0.1)
-
-print("All 0")
-strip.setcolor(0, 0, 0)
 # time.sleep(1)
 print("Cleanup")
 strip.cleanup()
